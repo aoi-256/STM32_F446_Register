42c65a6cdd9aacdc32cb04c3855332e55918b29f
aoi-256 <aoi256jp@Gmail.com>
2025-03-12 16:57:13 +0900

    Create main.c

---
+++ 02_USART/main.c
@@ -0,0 +1,105 @@
+#include "stm32f4xx.h"
+
+/* @brief USART2の初期設定
+ *
+ * @param BaudRate	USARTの通信速度(bps)を設定
+ *
+ * @return None
+ */
+void USART2_Init(uint32_t BaudRate){
+
+    //USART2にクロックを供給
+    RCC->APB1ENR |= 1 << 17;
+
+    //Baudrateを設定
+    USART2->BRR = 16000000 / BaudRate;
+
+	//GPIOAにクロックを供給
+    RCC->AHB1ENR |= 0x01;
+
+    //PA2とPA3にAlternateモードに設定
+    GPIOA->MODER |= 0b10 << 4;
+    GPIOA->MODER |= 0b10 << 6;
+
+    //PA2とPA3をUSART2に設定(AF7)
+    GPIOA->AFR[0] |= 0b0111 << 8;
+    GPIOA->AFR[0] |= 0b0111 << 12;
+
+    //PA2とPA3にPullUpを設定
+    GPIOA->PUPDR |= 0b01 << 4;
+    GPIOA->PUPDR |= 0b01 << 6;
+
+    //USART2RxとTxを有効化
+    USART2->CR1 |= 0b11 << 2;
+
+    //USART2を有効化
+    USART2->CR1 |= 1 << 13;
+}
+
+/* @brief USART2の送信
+ *
+ * @param Data 送信データのポインタ
+ * @param Len 　送信するデータ数
+ *
+ * @return None
+ */
+void USART2_Transmit(uint8_t* Data, uint8_t Len){
+
+	//データを1byteずつ送信
+	for(uint8_t i=0; i<Len; i++){
+
+		//DRレジスタを待機
+		while((USART2->SR & 0x80) == 0);
+
+		USART2->DR = Data[i];
+
+		//送信完了を待機
+		while((USART2->SR & 0x40) == 0);
+	}
+
+	//改行コードを送る
+	while((USART2->SR & 0x80) == 0);
+
+	USART2->DR = 10;
+
+	while((USART2->SR & 0x40) == 0);
+
+}
+
+/* @brief USART2の受信
+ *
+ * @param Data 受信バッファーのポインタ
+ * @param Len 　受信するデータ数
+ *
+ * @return None
+ */
+void USART2_Receive(uint8_t* Data, uint8_t Len){
+
+	//データを1byteずつ送信
+	for(uint8_t i=0; i<Len; i++){
+
+		//データの受信を待機
+		while((USART2->SR & 0x20) == 0);
+
+		Data[i] = USART2->DR;
+	}
+}
+
+int main(void){
+
+	//初期設定
+	USART2_Init(115200);
+
+	//データ
+	uint8_t DataBuffer[2];
+
+    while(1){
+
+    	//データの受信
+    	USART2_Receive(DataBuffer, 2);
+
+    	//データの送信
+    	USART2_Transmit(DataBuffer, 2);
+    }
+    return 0;
+}
